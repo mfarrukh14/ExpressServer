c9bc0b779d21439af7dc757b2cf662354cef03a7
mfarrukh14 <87035938+mfarrukh14@users.noreply.github.com>
2024-07-18 21:15:43 +0500

    Practiced on creating my first ever express server

---
+++ script.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+
+app.set("view engine","ejs");
+
+app.use(express.static('./public'))
+
+app.use(function(req,res,next){
+    console.log("middleware OK");
+    next();
+})
+
+app.get("/",function(req,res){
+    res.render('home');
+})
+
+app.get("/contact",function(req,res){
+    throw Error("Code: h1");
+});
+
+app.use(function(err,req,res,next){
+    if(res.headersSent){
+        return next(err);
+    }
+    res.status(500);
+    res.render('error', {error:err});
+});
+
+app.listen(3000);
+
